5cafe1b9a521548d22b1bc4833d44ca93e3a900e
Daniel Asamudo <dan.asamudo@gmail.com>
2025-12-26 00:53:35 -0700

    Initial Commit

---
+++ Pinterest Learning/Pinterest_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinterest_LearningApp.swift
+//  Pinterest Learning
+//
+//  Created by Daniel Asamudo on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct Pinterest_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
