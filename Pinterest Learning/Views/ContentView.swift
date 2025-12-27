1b274566297e22f3d69b3cf31ef0e0cdcca58b97
euclidiac <77080451+euclidiac@users.noreply.github.com>
2025-12-26 17:38:36 -0700

    Add files via upload

--- Pinterest Learning/Views/ContentView.swift
+++ Pinterest Learning/Views/ContentView.swift
@@ -1,22 +1,26 @@
 //
-//  ContentView.swift
-//  Pinterest Learning
-//
-//  Created by Daniel Asamudo on 2025-12-26.
-//
+    // Project: MacOSNotesApp
+    //  File: ContentView.swift
+    //  Created by Noah Carpenter
+    //  🐱 Follow me on YouTube! 🎥
+    //  https://www.youtube.com/@NoahDoesCoding97
+    //  Like and Subscribe for coding tutorials and fun! 💻✨
+    //  Fun Fact: Cats have five toes on their front paws, but only four on their back paws! 🐾
+    //  Dream Big, Code Bigger
+    
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var notes: [Note] = [
-        Note(title: "Hey there!", body: "Start writing here!")
-    ]
+    @StateObject private var store = NoteStore()
     @State private var selectedNote: Note?
+    
     var body: some View {
         NavigationSplitView{
             List(selection: $selectedNote) {
-                ForEach(notes){ note in
+                ForEach(store.notes){ note in
                     Text(note.title)
+                        .tag(note)
                 }
             }
             .navigationTitle("My Notes")
@@ -24,27 +28,27 @@ struct ContentView: View {
             .frame(minWidth: 200)
             .toolbar {
                 Button("New Note"){
-                    let newNote = Note(title: "New Note", body: "")
-                    notes.append(newNote)
+                    let newNote = Note(title: "Untitled", body: "")
+                    store.notes.append(newNote)
                     selectedNote = newNote
-                    
                 }
             }
         }
-        detail: {
+    detail:
+        {
             if let note = selectedNote {
-                SwiftUIView(note: binding(for: note))
-            } else {
-                Text("Choose a note.")
+                NoteDetailView(note: binding(for: note))
+            } else{
+                Text("Select a note")
                     .foregroundStyle(.secondary)
             }
         }
     }
     private func binding(for note: Note) -> Binding<Note> {
-        guard let index = notes.firstIndex(where: { $0.id == note.id}) else {
+        guard let index = store.notes.firstIndex(where: { $0.id == note.id}) else {
             fatalError("Note not found")
         }
-        return $notes[index]
+        return $store.notes[index]
     }
 }
 
